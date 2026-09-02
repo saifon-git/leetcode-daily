38c907f4fe0665dcf5fb54c458d51c36dcfdc5f8
saifmohammad <138210365+saifon-git@users.noreply.github.com>
2026-09-02 14:23:23 +0530

    Time: 0 ms (100.00%) | Memory: 15.5 MB (37.07%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int findPvt(vector<int>nums){
+        int n = nums.size();
+
+        int l = 0;
+        int r = n-1;
+
+        while(l<r){
+            int mid = l+(r-l)/2;
+            if(nums[mid]>nums[r])l=mid+1;
+            else r = mid;
+        }
+        return r;
+    }
+
+    int binarysearch(int l,int r,vector<int>nums,int target){
+        int idx = -1;
+
+        while(l<=r){
+            int mid = l+(r-l)/2;
+
+            if(nums[mid]==target)return mid;
+            else if(nums[mid]>target)r = mid-1;
+            else l = mid+1;
+            
+        }
+        return -1;
+    }
+
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+
+        int pivot_index = findPvt(nums);
+
+        int idx = binarysearch(0,pivot_index-1,nums,target);
+
+        if(idx != -1)return idx;
+
+        int idx1 = binarysearch(pivot_index,n-1,nums,target);
+        return idx1; 
+    }
+};

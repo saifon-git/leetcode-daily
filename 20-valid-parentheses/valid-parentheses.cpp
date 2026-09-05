cfe1036bda0c0b8f970442f56bc4e230e565763e
saifmohammad <138210365+saifon-git@users.noreply.github.com>
2026-09-06 02:58:42 +0530

    Time: 3 ms (9.24%) | Memory: 9 MB (18.19%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
     bool isValid(string s) {
-        int n = s.size();
+        
         stack<char>st;
 
         for(char ch : s){
-            if(st.empty() || ch == '[' || ch == '{' || ch == '('){
-                st.push(ch);
-                
-            }
-            if(ch == ']'){
-                if(st.top()=='['){
-                    st.pop();
-                }else return false;
-            }
-            if(ch == '}'){
-                if(st.top()=='{'){
-                    st.pop();
-                }else return false;
-            }
-            if(ch == ')'){
-                if(st.top()=='('){
-                    st.pop();
-                }else return false;
+            if(st.empty() || ch=='[' || ch=='{' || ch=='(')
+            st.push(ch);
+
+            else{
+                if(ch==']' && !st.empty() && st.top()=='[')st.pop();
+                else if(ch=='}' && !st.empty() && st.top()=='{')st.pop();
+                else if(ch==')' && !st.empty() && st.top()=='(')st.pop();
+
+                else return false;
             }
         }
         return st.empty();

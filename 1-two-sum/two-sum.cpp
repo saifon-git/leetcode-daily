d2f37efb5c06d34adba916bc9156a6a14637a9b1
saifmohammad <138210365+saifon-git@users.noreply.github.com>
2026-09-05 21:15:54 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (27.74%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -5,7 +5,6 @@ public:
         unordered_map<int,int>mp;
 
         for(int i=0;i<n;i++){
-
             int remain = target - nums[i];
 
             if(mp.find(remain)!=mp.end())return {mp[remain],i};

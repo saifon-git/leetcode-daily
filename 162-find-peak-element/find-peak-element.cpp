f774b6f1f3b1b45f605b3a4808c9eba4885bce16
saifmohammad <138210365+saifon-git@users.noreply.github.com>
2026-09-08 17:57:32 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (13.44%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -10,6 +10,6 @@ public:
         for(int i=1;i<n-1;i++){
             if(nums[i]>nums[i-1] && nums[i]>nums[i+1])return i;
         }
-        return n-1;
+        return nums[n-2]<nums[n-1]?n-1:n-2;
     }
 };
